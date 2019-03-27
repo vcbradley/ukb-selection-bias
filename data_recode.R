45742cee1c0c18c2f7bb5e4c8e8c5a98d3f45b5d
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-03-27 11:25:43 +0000

    more recodes

--- data_recode.R
+++ data_recode.R
@@ -5,6 +5,9 @@ setwd('/well/nichols/projects/UKB/SMS')
 # set location of full data file
 full_data_file = 'ukb25120.csv'
 
+# read in data header to get all names
+data_head = names(fread(full_data_file, nrows = 0))
+
 # read in list of variables
 variables = fread('/well/nichols/users/bwj567/mini-project-1/variable_codings.csv')
 variables = unique(variables[, .(cat, var, `biobank code`)])
@@ -12,13 +15,10 @@ setnames(variables, old = 'biobank code', new = 'biobank_code')
 
 all_parent_codes = variables[, biobank_code]
 
-# read in data header to get all names
-data_head = names(fread(full_data_file, nrows = 0))
-
 # grab list of actual variable names in the data (at baseline)
 vars_baseline = lapply(all_parent_codes, function(c){
-	data_head[grepl(paste0('^',c,'-0'), data_head)]
-	})
+    data_head[grepl(paste0('^',c,'-0'), data_head)]
+    })
 names(vars_baseline) = variables[, var]
 vars_baseline = unlist(vars_baseline)
 vars_baseline = data.table(var = names(vars_baseline), code = vars_baseline)
@@ -26,20 +26,51 @@ vars_baseline = data.table(var = names(vars_baseline), code = vars_baseline)
 
 # grab list of actual variable names in the data (at imaging)
 vars_imaging = lapply(all_parent_codes, function(c){
-	data_head[grepl(paste0('^',c,'-2'), data_head)]
-	})
+    data_head[grepl(paste0('^',c,'-2'), data_head)]
+    })
 names(vars_imaging) = variables[, var]
 vars_imaging = unlist(vars_imaging)
 vars_imaging = data.table(var = names(vars_imaging), code = vars_imaging)
 
 
 
+# read in ALL the imaging data
+data_imaging = fread(full_data_file
+    , nrows = 5000     #for testing
+    , select = c('eid', vars_imaging$code)
+    , col.names = c('eid', vars_imaging$var)
+    , na.strings = ''
+    )
+data_imaging
 
-# read in ALL the data
-data = fread('ukb25120.csv'
-	, nrows = 0 	#for testing
-	, select = c("6138-0.0", "6138-0.1","6138-0.2","6138-0.3","6138-0.4","6138-0.5")
-	)
+# check imaging variables
+data_imaging[, .N, is.na(brain_vol)]
+data_imaging[, .N, .(has_brain_vol = !is.na(brain_vol), MRI_completed == '1')]
+data_imaging[, .N, .(has_brain_vol = !is.na(brain_vol), MRI_completed == '1')]
+
+data_imaging[, .N, assessment_center]
+
+
+
+# read in ALL the imaging data
+data_baseline = fread(full_data_file
+    , nrows = 5000     #for testing
+    , select = c('eid', vars_baseline$code)
+    , col.names = c('eid', vars_baseline$var)
+    , na.strings = ''
+    )
+data_baseline
+
+data_baseline[, ]
+
+
+###########
+# RECODES #
+###########
+
+#
+
+
 
 ## VAR-INSTANCE.MEASUREMENT
 data[, .(.N,.N/nrow(data)), by = `6138-0.0`][order(`6138-0.0`)]
