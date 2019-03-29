9b082b6c5978bd6f00f75ac42364d93944b78111
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-03-29 14:49:57 +0000

    WORKING!

---
+++ prep_ukb/data_extract.sh
@@ -0,0 +1,51 @@
+##data_extract.sh
+
+cd /well/nichols/users/bwj567/
+
+
+
+# SET PARAMS
+INSTANCE='baseline'
+#FILE_IN='/well/nichols/projects/UKB/SMS/ukb25120.csv'
+FILE_IN='/well/nichols/users/bwj567/data/ukb25120_sample5k.csv'
+FILE_OUT='/well/nichols/users/bwj567/ukb25120'
+if [ "$INSTANCE" != "baseline" ]
+then 
+	FILE_OUT=$FILE_OUT'_imaging.tsv'
+else
+	FILE_OUT=$FILE_OUT'_baseline.tsv'
+fi
+echo $FILE_OUT
+echo $FILE_IN
+
+
+
+# CREATE VARIABLE LIST
+
+# grab list of variables and cat to new file
+head mini-project-1/variable_codings.csv
+cat mini-project-1/variable_codings.csv | cut -d ',' -f3 > data/vars.csv
+
+# remove header
+sed -i '1d' data/vars.csv
+sed -i '1d' data/vars.csv
+
+# remove blank rows
+sed -i '/^$/d' data/vars.csv
+
+cat data/vars.csv
+
+
+##### USING UKBparse
+# --variables/-v = which vars to select.  can select with a file vars.csv
+# --subject/-s = the IDs of the subjects to select.  can select with ranges (i.e. 1:5), from a file (i.e. subj.txt) or by variable value (i.e. "v1 > 10 && v2 < 70")
+# --visit/-vi = the visit to select. select by number (i.e. 1) or with "last"
+# -nb = don't use UKB recodes
+# -q = quiet
+# -ow = force overwrite existing out file
+
+# select only sec
+
+
+ukbparse -nb --log_file /well/nichols/users/bwj567/log.txt -v data/vars.csv -vi 0 $FILE_OUT $FILE_IN 
+
