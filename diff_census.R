58d82a1901f1c67a23cc5321e1fd56e4895bec49
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-04-01 15:55:25 +0100

    initial commit census analysis

---
+++ diff_census.R
@@ -0,0 +1,11 @@
+######## Analysis of differences
+
+## data recode
+library('data.table')
+library('memisc')
+library('dplyr')
+library('stringr')
+setwd('/well/nichols/users/bwj567/data')
+
+
+ukbdata = fread()
