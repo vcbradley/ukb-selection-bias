cdd9f0580009cffcbd96db6685a5e57d5e4aa71d
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-04-14 18:16:28 +0100

    updates for imaging at time of imaging

--- analysis/summary_xtabs.R
+++ analysis/summary_xtabs.R
@@ -14,6 +14,9 @@ source('mini-project-1/analysis/functions.R')
 ukbdata = fread('data/ukb25120_weighting_base.csv')
 ukbdata <- as_tibble(ukbdata)
 
+ukbimgdata = fread('data/ukb25120_weighting_img.csv')
+ukbimgdata <- as_tibble(ukbimgdata)
+
 censusdata = fread('data/census11_recoded.csv')
 censusdata <- as_tibble(censusdata)
 
@@ -27,11 +30,12 @@ hsedata11 <- as_tibble(hsedata11)
 censusdata <- censusdata %>% filter(demo_age_bucket >= '40 to 44') %>% filter(demo_age_bucket <= '65 to 69')
 
 # limit HSE people to 40 to 70
-hsedata <- hsedata %>% filter(demo_age_bucket >= '40 to 44') %>% filter(demo_age_bucket <= '65 to 69')
+hsedata <- hsedata %>% filter(demo_age_bucket >= '40 to 44') %>% filter(demo_age_bucket <= '80 to 84')
 hsedata11 <- hsedata11 %>% filter(demo_age_bucket >= '40 to 44') %>% filter(demo_age_bucket <= '65 to 69')
 
 
 ukbdata %>% group_by(base_demo_age_bucket) %>% tally()
+ukbimgdata %>% group_by(img_demo_age_bucket) %>% tally()
 censusdata %>% group_by(demo_age_bucket) %>% tally()
 hsedata %>% group_by(demo_age_bucket) %>% tally()
 hsedata11 %>% group_by(demo_age_bucket) %>% tally()
@@ -46,13 +50,17 @@ hsedata11 %>% select(., grep('wt', names(hsedata11)))
 
 
 # set list of vars
-varlist = c(names(ukbdata)[grepl('base_demo', names(ukbdata))], names(ukbdata)[grepl('base_health', names(ukbdata))])
+varlist_base = c(names(ukbdata)[grepl('base_demo', names(ukbdata))], names(ukbdata)[grepl('base_health', names(ukbdata))])
+varlist_img = c(names(ukbimgdata)[grepl('img_demo', names(ukbimgdata))], names(ukbimgdata)[grepl('img_health', names(ukbimgdata))])
 
 #### DO UKB SUMMARY
-summary_base = getAllSummaries(data = ukbdata, varlist = varlist, suffix = 'ukb')
+summary_base = getAllSummaries(data = ukbdata, varlist = varlist_base, suffix = 'ukb')
 
 #### DO IMG SUMMARY
-summary_img = getAllSummaries(data = ukbdata %>% filter(img_has_t1_MRI == 1), varlist = varlist, suffix = 'ukb_img')
+summary_img = getAllSummaries(data = ukbdata %>% filter(img_has_t1_MRI == 1), varlist = varlist_base, suffix = 'ukb_img')
+
+#### DO IMG AT TIME OF IMAGING SUMMARY
+summary_img_atimg = getAllSummaries(data = ukbimgdata %>% filter(img_has_t1_MRI == 1), varlist = varlist_img, suffix = 'ukb_img_atimg')
 
 #### DO CENSUS SUMMARY
 summary_census = getAllSummaries(data = censusdata, varlist = names(censusdata)[grepl('demo|health', names(censusdata))], suffix = 'census')
@@ -65,7 +73,7 @@ summary_hse11 = getAllSummaries(data = hsedata11, varlist = names(hsedata11)[gre
 
 
 #### MERGE ALL TOGETHER
-summary_full = Reduce(function(x, y) merge(x, y, by=c("var", 'level'), all = T), list(summary_img, summary_base, summary_census, summary_hse11, summary_hse))
+summary_full = Reduce(function(x, y) merge(x, y, by=c("var", 'level'), all = T), list(summary_img, summary_base, summary_img_atimg, summary_census, summary_hse11, summary_hse))
 
 #### Rearrage columns
 col_order = c('var', 'level'
@@ -80,7 +88,7 @@ summary_full = summary_full[, col_order, with = F]
 summary_full[, diff_ukb := dist_ukb_img - dist_ukb]
 summary_full[, diff_census := dist_ukb_img - dist_census]
 summary_full[, diff_hse11 := dist_ukb_img - dist_hse11]
-summary_full[, diff_hse16 := dist_ukb_img - dist_hse16]
+summary_full[, diff_hse16 := dist_ukb_img_atimg - dist_hse16]
 
 
 ##### Write out to file
