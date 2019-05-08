366abb2f70ed4ad74ba61f30101d53726eb5b4f1
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-05-08 22:08:07 +0100

    simulation updates

--- simulation/simulation_draft.R
+++ simulation/simulation_draft.R
@@ -1,19 +1,22 @@
 #### SIMULATION
 
 library(stringr)
+library(knitr)
+library(randomForest)
 
 
 setwd('/well/nichols/users/bwj567')
 source('mini-project-1/weighting/weighting_functions.R')  #also loads lots of packages
 
 # Read in UKB data
-ukbdata = fread('data/ukb25120_weighting_img.csv')
-ukbdata <- as_tibble(ukbdata)
+ukbdata = fread('data/ukb25120_weighting_img.csv', stringsAsFactors = F)
 
-# drop img prefix
-ukbdata <- ukbdata %>% rename_at(vars(starts_with("img_")), funs(str_replace(., "img_", "")))
+setnames(ukbdata
+    , old = names(ukbdata)[grepl('^img', names(ukbdata))]
+    , new = gsub('^img_','',names(ukbdata)[grepl('^img', names(ukbdata))])
+    )
 
-ukbdata = data.table(ukbdata, stringsAsFactors = F)
+names(ukbdata)
 
 #mean impute missing BMIs
 ukbdata[is.na(bmi), bmi := mean(ukbdata$bmi, na.rm = T)]
@@ -32,9 +35,23 @@ nas = apply(ukbdata, 2, function(x) sum(is.na(x)))
 nas[nas > 0]
 
 
+#summarize vars
+varsum = apply(ukbdata[, vars_to_consider, with = F], 2, function(x) {
+    if(length(unique(x)) < 20) {
+        paste0(sort(unique(x)), collapse = ', ')
+    } else {
+        paste0(x[1:5], collapse = ', ')
+    }
+    })
+head(varsum)
+varsum = data.frame(variable = names(varsum), levels = varsum)
+rownames(varsum) = NULL
+kable(varsum, format = 'latex')
+
+
 
 #### Probability of missingness
-vars_to_consider = names(ukbdata)[-grep('^MRI|eid|has|assessment|demo_ethnicity_4way|demo_white', names(ukbdata))]
+vars_to_consider = names(ukbdata)[-grep('^MRI|eid|has|assessment|demo_ethnicity_4way|demo_white|demo_educ_highest$', names(ukbdata))]
 formula = paste("~-1+(", paste0(vars_to_consider, collapse = " + "), ")")
 ukbdata_modmat = modmat_all_levs(as.formula(formula), data = ukbdata[1:5000,])
 
@@ -68,12 +85,14 @@ coeff_samples = rbindlist(lapply(missingness_covars$data_type, function(t, n_sam
     # always include noise
     if(t == 'noise'){
         spike = rep(1, n_samples)
+        slab_sd = 0.25
     }else{
         spike_prob = ifelse(t == 'int', 0.5, 0.1)
         spike = rbinom(n = n_samples, size = 1, prob = spike_prob)
+        slab_sd = ifelse(t == 'int', 1.5, 2)
     }  
 
-    slab_sd = ifelse(t == 'int', 1.5, 2)
+    
     slab = rnorm(n = n_samples, 0, slab_sd)
 
     data.frame(t(spike * slab))
@@ -101,6 +120,9 @@ apply(samples, 2, sum)
 #how'd we do on sampling different peeps?
 apply(samples, 1, sum)
 
+# number of vars used on avg
+apply(coeff_samples, 2, function(x) sum(x != 0))
+
 
 sample = ukbdata[1:5000,][samples[,1] == 1,]
 
@@ -127,21 +149,62 @@ vars = c('demo_sex'
         , 'demo_hh_accom_type'
         )
 
+
+###### RAKING
 raked_data = doRaking(svydata = sample
     , popdata = ukbdata
     , vars = vars
     )
 
 
-# select vars with a regression?
-
-cv.glmnet(ukbdata_modmat[, -which(colnames(ukbdata_modmat) %in% c('noise'))])
 
 
-strat_data = doPostStrat(svydata
-	, popdata
-	, vars)
+####### POST STRAT
 
+# make model matrix with categorical vars for random forest
+ps_modmat = ukbdata[1:5000, vars, with = F]
+ps_modmat[,(vars):=lapply(.SD, as.factor),.SDcols=vars]
+
+# fit random forest and calc variable importance
+ps_fit = randomForest(y = samples[,1], x = ps_modmat, importance = T)
+ps_vars = sort(ps_fit$importance[, 1], decreasing = T)
+
+### Increase number of stratification variables until we lose too much of the pop
+prop_pop_dropped = 0
+n_vars = 2
+# implicit tolerance threshold for dropped strata
+while(prop_pop_dropped < 0.01){
+    # get strat vars
+    strat_vars = names(ps_vars[1:n_vars])
+
+    # calculate number in sample and number in populatioin
+    drop_pop = merge(ukbdata[1:5000, .(prop_pop = .N/nrow(ukbdata[1:5000])), by = strat_vars]
+        , sample[, .(n_samp = .N, prop_samp = .N/nrow(sample)), by = strat_vars], all.x = T)
+
+    # caluclate the pct of the sample that will be dropped
+    prop_pop_dropped = drop_pop[is.na(n_samp), sum(prop_pop)]
+
+    #increment number of variables
+    n_vars = n_vars + 1
+}
+# take one fewer than the number it took to go over the tol threshold
+n_vars = n_vars - 1
+strat_vars = names(ps_vars[1:n_vars])
+
+## DO POST STRAT
+drop_pop = merge(ukbdata[1:5000, .(prop_pop = .N/nrow(ukbdata[1:5000])), by = strat_vars]
+        , sample[, .(n_samp = .N, prop_samp = .N/nrow(sample)), by = strat_vars], all.x = T)
+
+drop_pop[, weight := prop_pop/prop_samp]
+strat_data = merge(sample, drop_pop, by = strat_vars, all.x = T)
+
+
+#spot check
+strat_data[, .(.N/nrow(strat_data), sum(weight, na.rm = T)/sum(strat_data$weight, na.rm = T)), demo_hh_size]
+ukbdata[1:5000, .N/nrow(ukbdata[1:5000]), demo_hh_size]
+
+strat_data[, .(.N/nrow(strat_data), sum(weight, na.rm = T)/sum(strat_data$weight, na.rm = T)), demo_age_bucket]
+ukbdata[1:5000, .N/nrow(ukbdata[1:5000]), demo_age_bucket]
 
 
 
