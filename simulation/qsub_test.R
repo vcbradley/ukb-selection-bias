7d0bbeb69cd7ebeef128db625d244d64ff2fd447
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-05-21 16:08:51 -0400

    adding test script

---
+++ simulation/qsub_test.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+#$ -S /usr/local/packages/R-3.5.1/bin/Rscript
+#$ -t 1:10
+#$ -cwd   # this is so that you stay in the directory that you launched from
+#$ -o $HOME/logs   #redirecting std out and std error
+#$ -e $HOME/logs
+
+JobId=as.numeric(Sys.getenv("SGE_TASK_ID"))
+
+write(JobId,file=sprintf("Run%03d.dat",JobId))
+
