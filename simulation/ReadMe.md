9d8f576384eee7a91ddab9ca80e4150031311a14
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-05-23 14:27:48 -0400

    ugh

--- simulation/ReadMe.md
+++ simulation/ReadMe.md
@@ -16,3 +16,7 @@ Files:
 i.e. 
 `cd /well/nichols/users/bwj567/simulation/sim_1_5000_1`
 `qsub /well/nichols/users/bwj567/mini-project-1/simulation/simulation_sample_gen.R`
+4. `qsub /well/nichols/users/bwj567/mini-project-1/simulation/simulation_run.R`
+
+To check on job status, `qstat`
+To delete job, `qdel JOBID`
--- simulation/simulation_sample_gen.R
+++ simulation/simulation_sample_gen.R
@@ -32,58 +32,56 @@ prop_sampled_options = c(0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 0.75)
 
 # Set specific param for this task
 TaskId = as.numeric(Sys.getenv("SGE_TASK_ID"))
-#prop_sampled = prop_sampled_options[TaskId]
+prop_sampled = prop_sampled_options[TaskId]
 
-# load('data_modmat.rda')
-# coeff_samples = fread('coefs.csv')
+load('data_modmat.rda')
+coeff_samples = fread('coefs.csv')
 
-write.csv(prop_sampled_options, file = paste('test', TaskId, '.csv'))
-
-# ####### USE COEFS TO GENERATE SAMPLES
-# lps = ukbdata_modmat %*% as.matrix(coeff_samples)
-# probs = exp(lps)/(1+exp(lps))
+####### USE COEFS TO GENERATE SAMPLES
+lps = ukbdata_modmat %*% as.matrix(coeff_samples)
+probs = exp(lps)/(1+exp(lps))
 
 
-# samples = lapply(1:ncol(probs), function(i, probs, prop_sampled, n_samples){
-#     p = probs[,i]
-#     temp = matrix(1, nrow = length(p), ncol = n_samples)
+samples = lapply(1:ncol(probs), function(i, probs, prop_sampled, n_samples){
+    p = probs[,i]
+    temp = matrix(1, nrow = length(p), ncol = n_samples)
 
-#     temp = apply(temp, 2, function(p, prop_sampled){
-#         sampled_int = sample.int(n = length(p), size = length(p) * prop_sampled, prob = p)
-#         sampled = seq(from = 1, to = length(p), by = 1)
-#         sampled = ifelse(sampled %in% sampled_int, 1, 0)
-#         }, prop_sampled)
+    temp = apply(temp, 2, function(p, prop_sampled){
+        sampled_int = sample.int(n = length(p), size = length(p) * prop_sampled, prob = p)
+        sampled = seq(from = 1, to = length(p), by = 1)
+        sampled = ifelse(sampled %in% sampled_int, 1, 0)
+        }, prop_sampled)
 
-#     return(data.table(temp))
+    return(data.table(temp))
     
-#     }, probs, prop_sampled, n_samples)
-# samples = do.call(cbind, samples)
+    }, probs, prop_sampled, n_samples)
+samples = do.call(cbind, samples)
 
-# length(samples)
-# dim(samples)
-# head(samples)
+length(samples)
+dim(samples)
+head(samples)
 
-# #check that we sampled the right number
-# apply(samples, 2, sum)
+#check that we sampled the right number
+apply(samples, 2, sum)
 
-# #how'd we do on sampling different peeps?
-# summary(apply(samples, 1, sum))
-
-
-
-# ######### Write out samples to directory to run simulation
-# sample_dir = paste0('samples/prop_', prop_sampled)
-# dir.create(sample_dir)
+#how'd we do on sampling different peeps?
+summary(apply(samples, 1, sum))
 
 
-# lapply(1:ncol(samples), function(s){
-# 	samp = samples[, s, with = F]
-# 	filename = paste0(sample_dir, '/sample_', str_pad(s, width = 5, side = 'left', pad = '0'), '.csv')
+
+######### Write out samples to directory to run simulation
+sample_dir = paste0('samples/prop_', prop_sampled)
+dir.create(sample_dir)
+
+
+lapply(1:ncol(samples), function(s){
+	samp = samples[, s, with = F]
+	filename = paste0(sample_dir, '/sample_', str_pad(s, width = 5, side = 'left', pad = '0'), '.csv')
 	
-# 	write.csv(samp, file = filename, row.names = F)
+	write.csv(samp, file = filename, row.names = F)
 
-# 	filename
-# 	})
+	filename
+	})
 
 
 
