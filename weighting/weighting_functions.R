1919437f18e2ddcfab88ac6f3c47c64596278f8f
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-06-09 15:33:28 +0100

    add in pop weight col for calibration

--- weighting/weighting_functions.R
+++ weighting/weighting_functions.R
@@ -451,7 +451,14 @@ doLassoRake = function(
 }
 
 
-doCalibration = function(svydata, popdata, vars, epsilon = 1, calfun = 'raking'){
+doCalibration = function(svydata, popdata, vars, epsilon = 1, calfun = 'raking', pop_weight_col = NULL){
+
+    if(is.null(pop_weight_col)){
+        popdata[, pop_weight := 1]
+    }else{
+        popdata[, pop_weight := get(pop_weight_col)]
+        popdata[-which(is.na(pop_weight)),]
+    }
 
     # check levels and get rid of those vars with only 1 so model matrix works
     samp_levels = apply(svydata[, vars, with = F], 2, function(x) length(unique(x)))
@@ -489,18 +496,18 @@ doCalibration = function(svydata, popdata, vars, epsilon = 1, calfun = 'raking')
     colnames(samp_modmat) = cal_vars$var_code[which(cal_vars$var_name %in% colnames(samp_modmat))]
 
     ## calculate sample and population totals
-    pop_totals = apply(pop_modmat, 2, sum)
+    pop_totals = apply(pop_modmat, 2, function(x, pop_weight) {sum(x * pop_weight, na.rm = T)}, pop_weight = popdata$pop_weight)
     samp_totals = apply(samp_modmat, 2, sum)
     pop_levels = apply(pop_modmat, 2, function(x) length(unique(x)))
 
     # re-scale continuous vars
-    cols = which(pop_totals > nrow(popdata))
+    cols = which(pop_totals > sum(popdata$pop_weight))
     if(length(cols) > 0){
         if(length(cols) == 1){
-            pop_modmat[,cols] <- pop_modmat[,cols] / sum(pop_modmat[,cols]) * nrow(popdata)
+            pop_modmat[,cols] <- pop_modmat[,cols] / sum(pop_modmat[,cols]) * sum(popdata$pop_weight)
             samp_modmat[,cols] <- samp_modmat[,cols] / sum(samp_modmat[,cols]) * nrow(svydata)
         }else{
-            pop_modmat[,cols] <- apply(pop_modmat[,cols], 2, function(x) x / sum(x) * nrow(popdata))
+            pop_modmat[,cols] <- apply(pop_modmat[,cols], 2, function(x) x / sum(x) * sum(popdata$pop_weight))
             samp_modmat[,cols] <- apply(samp_modmat[,cols], 2, function(x) x / sum(x) * nrow(svydata))
         }
 
@@ -511,12 +518,12 @@ doCalibration = function(svydata, popdata, vars, epsilon = 1, calfun = 'raking')
 
 
     # recalc totals
-    pop_totals = apply(pop_modmat, 2, sum)
+    pop_totals = apply(pop_modmat, 2, function(x, pop_weight) {sum(x * pop_weight, na.rm = T)}, pop_weight = popdata$pop_weight)
     samp_totals = apply(samp_modmat, 2, sum)
     pop_levels = apply(pop_modmat, 2, function(x) length(unique(x)))
 
     ### DROP more levels that are too small
-    small_pop_strata = pop_totals/nrow(pop_modmat)
+    small_pop_strata = pop_totals/sum(popdata$pop_weight)
     small_pop_strata = small_pop_strata[(small_pop_strata < 0.02 | small_pop_strata > 0.98 & small_pop_strata < 1) & pop_levels <= 2]
     small_pop_strata = data.table(var_code = names(small_pop_strata), pop_prop = small_pop_strata)
 
@@ -536,7 +543,7 @@ doCalibration = function(svydata, popdata, vars, epsilon = 1, calfun = 'raking')
 
     
     ## re-calc pop totals
-    pop_totals = apply(pop_modmat, 2, sum)
+    pop_totals = apply(pop_modmat, 2, function(x, pop_weight) {sum(x * pop_weight, na.rm = T)}, pop_weight = popdata$pop_weight)
     print(pop_totals)
 
     apply(samp_modmat, 2, function(x) length(unique(x)))
@@ -549,10 +556,11 @@ doCalibration = function(svydata, popdata, vars, epsilon = 1, calfun = 'raking')
     
     # split into subgroups for calibration so it doesn't die
     n_vars = length(pop_totals)
+    n_loop = ceiling(n_vars/20)
     
-    for(i in 1:2){
+    for(i in 1:n_loop){
         cat(paste0(Sys.time(), "\t\t\t Iteration ",i ,"\n"))
-        group = order(pop_totals)[ceiling((1:n_vars)/(n_vars/2)) == i]
+        group = order(pop_totals)[ceiling((1:n_vars)/(n_vars/n_loop)) == i]
 
         ## make formula
         formula_cal = as.formula(paste0('~ -1 +', paste(names(pop_totals)[group], collapse = '+')))
