996247d3c09720409f5658465e6409a3d6dccb00
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-06-12 14:32:36 +0100

    add rms

--- simulation/simulation_apoe_regressions_results.sh
+++ simulation/simulation_apoe_regressions_results.sh
@@ -4,17 +4,20 @@ cd /well/nichols/users/bwj567/mini-project-1/simulation/results/sim_1_5000_old_v
 ls apoe_reg_coef_prop_* -l
 
 ##### COEFS
-head -1 apoe_reg_coef_prop_0.01.rda > apoe_reg_coef_all.csv
+rm apoe_reg_coef_all.csv
+head -1 apoe_reg_coef_prop_0.01.csv > apoe_reg_coef_all.csv
 for filename in $(ls apoe_reg_coef_prop_*); do sed 1d $filename >> apoe_reg_coef_all.csv; done
 
 
+head apoe_reg_coef_all.csv
 tail apoe_reg_coef_all.csv
 
 ##### PVALS
 ls apoe_reg_pval_prop_*
 
-head -1 apoe_reg_pval_prop_0.01.rda > apoe_reg_pval_all.csv
+rm apoe_reg_pval_all.csv
+head -1 apoe_reg_pval_prop_0.01.csv > apoe_reg_pval_all.csv
 for filename in $(ls apoe_reg_pval_prop_*); do sed 1d $filename >> apoe_reg_pval_all.csv; done
 
 
-tail apoe_reg_pval_all.csv
+head apoe_reg_pval_all.csv
