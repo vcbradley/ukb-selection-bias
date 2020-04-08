5214005d33483085dfa0a3248c866194335a4636
vcbradley <valerie.bradley@new.ox.ac.uk>
2020-04-08 08:36:04 +0100

    drop age

--- simulation/simulation_apoe_regressions.R
+++ simulation/simulation_apoe_regressions.R
@@ -75,7 +75,8 @@ for(p in sort(unique(all_weights_demos[, prop_sampled]))){
 			
 			fit = glm(MRI_brain_vol_scaled ~
 				#demo_sex * health_apoe_phenotype +
-				age + age_sq
+				#age + 
+				age_sq
 				, data = all_weights_demos[prop_sampled == p & sim_num == i,]
 				, weights = weights
 			)
@@ -101,7 +102,8 @@ coefs_full
 p_vals_full[, V1 := NULL]
 
 ## pop
-pop_fit = glm(MRI_brain_vol_scaled ~ age + age_sq
+pop_fit = glm(MRI_brain_vol_scaled ~ #age + 
+	age_sq
 		, data = ukbdata
 	)
 pop_coefs = data.table(prop_sampled = p, sim_num = i, method = m, t(coef(pop_fit)))
